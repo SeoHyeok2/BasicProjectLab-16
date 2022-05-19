0b25eece4d4fce85b3c3e109d7f4e75170948767
rumyakun <artificialascent02@gmail.com>
2022-05-19 15:19:50 +0900

    UI 대부분 구현

---
+++ app/src/main/java/org/techtown/basicprojectex/CameraSelect.java
@@ -0,0 +1,13 @@
+package org.techtown.basicprojectex;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CameraSelect extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cameraselect);
+    }
+}
