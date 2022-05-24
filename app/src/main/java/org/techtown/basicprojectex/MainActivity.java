77853f2dc9aacf29622b1a0b9b14685e616c0b75
seohyeok <edward0802@naver.com>
2022-05-25 00:44:43 +0900

    merge camera model

--- app/src/main/java/org/techtown/basicprojectex/MainActivity.java
+++ app/src/main/java/org/techtown/basicprojectex/MainActivity.java
@@ -1,123 +1,185 @@
 package org.techtown.basicprojectex;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.content.res.AssetFileDescriptor;
-import android.os.Bundle;
-import android.view.View;
-
-import org.tensorflow.lite.Interpreter;
-
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
+import org.techtown.basicprojectex.classifier.ImageClassifier;
+import org.techtown.basicprojectex.R;
+
+/**
+ * The Main Activity Class
+ * <p>
+ * Created by A Anand on 11-05-2020
+ */
 public class MainActivity extends AppCompatActivity {
 
-    private static final int FROM_ALBUM = 1;    // onActivityResult 식별자
-    private static final int FROM_CAMERA = 2;   // 카메라는 사용 안함
+    /**
+     * Requests Codes to identify camera and permission requests
+     */
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 1000;
+    private static final int CAMERA_REQEUST_CODE = 10001;
+
+    /**
+     * UI Elements
+     */
+    private ImageView imageView;
+    private ListView listView;
+    private ImageClassifier imageClassifier;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 인텐트의 결과는 onActivityResult 함수에서 수신.
-        // 여러 개의 인텐트를 동시에 사용하기 때문에 숫자를 통해 결과 식별(FROM_ALBUM 등등)
-        findViewById(R.id.button_1).setOnClickListener(new View.OnClickListener() {
+        // initalizing ui elements
+        initializeUIElements();
+    }
+
+    /**
+     * Method to initalize UI Elements. this method adds the on click
+     */
+    private void initializeUIElements() {
+        imageView = findViewById(R.id.iv_capture);
+        listView = findViewById(R.id.lv_probabilities);
+        Button takepicture = findViewById(R.id.bt_take_picture);
+
+        /*
+         * Creating an instance of our tensor image classifier
+         */
+        try {
+            imageClassifier = new ImageClassifier(this);
+        } catch (IOException e) {
+            Log.e("Image Classifier Error", "ERROR: " + e);
+        }
+
+        // adding on click listener to button
+        takepicture.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.setType("image/*");                      // 이미지만
-                intent.setAction(Intent.ACTION_GET_CONTENT);    // 카메라(ACTION_IMAGE_CAPTURE)
-                startActivityForResult(intent, FROM_ALBUM);
+            public void onClick(View v) {
+                // checking whether camera permissions are available.
+                // if permission is avaialble then we open camera intent to get picture
+                // otherwise reqeusts for permissions
+                if (hasPermission()) {
+                    openCamera();
+                } else {
+                    requestPermission();
+                }
             }
         });
     }
 
-    private Interpreter getTfliteInterpreter(String modelPath) {
-        try {
-            return new Interpreter(loadModelFile(MainActivity.this, modelPath));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public MappedByteBuffer loadModelFile(Activity activity, String modelPath) throws IOException {
-        AssetFileDescriptor fileDescriptor = activity.getAssets().openFd(modelPath);
-        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
-        FileChannel fileChannel = inputStream.getChannel();
-        long startOffset = fileDescriptor.getStartOffset();
-        long declaredLength = fileDescriptor.getDeclaredLength();
-        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
-    }
-
-    // 사진첩에서 사진 파일 불러와 빈자리 유무 분류하는 코드
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // 카메라를 다루지 않기 때문에 앨범 상수에 대해서 성공한 경우에 대해서만 처리
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode != FROM_ALBUM || resultCode != RESULT_OK)
-            return;
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        // if this is the result of our camera image request
+        if (requestCode == CAMERA_REQEUST_CODE) {
+            // getting bitmap of the image
+            Bitmap photo = (Bitmap) Objects.requireNonNull(Objects.requireNonNull(data).getExtras()).get("data");
+            // displaying this bitmap in imageview
+            imageView.setImageBitmap(photo);
 
-        //각 모델에 따른 input , output shape 각자 맞게 변환
-        // mobilenetcheck.h5 일시 224 * 224 * 3
-        float[][][][] input = new float[1][224][224][3];
-        float[][] output = new float[1][2]; //tflite에 버섯 종류 2개라서 (내기준)
+            // pass this bitmap to classifier to make prediction
+            List<ImageClassifier.Recognition> predicitons = imageClassifier.recognizeImage(
+                    photo, 0);
 
-        try {
-            int batchNum = 0;
-            InputStream buf = getContentResolver().openInputStream(data.getData());
-            Bitmap bitmap = BitmapFactory.decodeStream(buf);
-            buf.close();
-
-            //이미지 뷰에 선택한 사진 띄우기
-            ImageView iv = findViewById(R.id.image);
-            iv.setScaleType(ImageView.ScaleType.FIT_XY);
-            iv.setImageBitmap(bitmap);
-
-            // x,y 최댓값 사진 크기에 따라 달라짐 (조절 해줘야함)
-            for (int x = 0; x < 224; x++) {
-                for (int y = 0; y < 224; y++) {
-                    int pixel = bitmap.getPixel(x, y);
-                    input[batchNum][x][y][0] = Color.red(pixel) / 1.0f;
-                    input[batchNum][x][y][1] = Color.green(pixel) / 1.0f;
-                    input[batchNum][x][y][2] = Color.blue(pixel) / 1.0f;
+            // creating a list of string to display in list view
+            final List<String> predicitonsList = new ArrayList<>();
+            for (ImageClassifier.Recognition recog : predicitons) {
+//              predicitonsList.add(recog.getName() + "  ::::::::::  " + recog.getConfidence());
+                if(recog.getConfidence() > 0) {
+                    predicitonsList.add("주차장 상태가 " + recog.getName() + "  입니다.");
                 }
             }
 
-            // 자신의 tflite 이름 써주기
-            Interpreter lite = getTfliteInterpreter("converted_model.tflite");
-            lite.run(input, output);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            // creating an array adapter to display the classification result in list view
+            ArrayAdapter<String> predictionsAdapter = new ArrayAdapter<>(
+                    this, R.layout.support_simple_spinner_dropdown_item, predicitonsList);
+            listView.setAdapter(predictionsAdapter);
 
-        TextView tv_output = findViewById(R.id.tv_output);
-        int i;
-
-        // 텍스트뷰에 무슨 버섯인지 띄우기 but error남
-        for (i = 0; i < 2; i++) {
-            if (output[0][i] * 100 > 90) {
-                if (i == 0) {
-                    tv_output.setText(String.format("빈자리 없음  %d %.5f", i, output[0][0] * 100));
-                }
-                else {
-                    tv_output.setText(String.format("빈자리 있음, %d, %.5f", i, output[0][1] * 100));
-                }
-            } else
-                continue;
         }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // if this is the result of our camera permission request
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (hasAllPermissions(grantResults)) {
+                openCamera();
+            } else {
+                requestPermission();
+            }
+        }
+    }
+
+    /**
+     * checks whether all the needed permissions have been granted or not
+     *
+     * @param grantResults the permission grant results
+     * @return true if all the reqested permission has been granted,
+     * otherwise returns false
+     */
+    private boolean hasAllPermissions(int[] grantResults) {
+        for (int result : grantResults) {
+            if (result == PackageManager.PERMISSION_DENIED)
+                return false;
+        }
+        return true;
+    }
+
+    /**
+     * Method requests for permission if the android version is marshmallow or above
+     */
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // whether permission can be requested or on not
+            if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                Toast.makeText(this, "Camera Permission Required", Toast.LENGTH_SHORT).show();
+            }
+            // request the camera permission permission
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    /**
+     * creates and starts an intent to get a picture from camera
+     */
+    private void openCamera() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQEUST_CODE);
+    }
+
+    /**
+     * checks whether camera permission is available or not
+     *
+     * @return true if android version is less than marshmallo,
+     * otherwise returns whether camera permission has been granted or not
+     */
+    private boolean hasPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+        }
+        return true;
     }
 }
